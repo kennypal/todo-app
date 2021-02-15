66c9b11740adab75b0830050e91083ae79f87ac8
Kenneth Palces <kennypal@kenneths-mbp.myfiosgateway.com>
2021-02-15 16:42:02 -0500

    added form

--- react_todo_app/src/App.js
+++ react_todo_app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <header>
+        <h1>Kenny's Todo List</h1>
+        <Form />
+      </header>
     </div>
   );
 }

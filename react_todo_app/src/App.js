ccd966e7d94d4dad0bdc4ccc4d377348f10692ba
Kenneth Palces <kennypal@kenneths-mbp.myfiosgateway.com>
2021-02-18 18:28:26 -0500

    prevent default

--- react_todo_app/src/App.js
+++ react_todo_app/src/App.js
@@ -5,12 +5,13 @@ import TodoList from './components/TodoList'
 
 function App() {
   const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
       <header>
         <h1>Kenny's Todo List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText}/>
       <TodoList />
     </div>
   );
--- react_todo_app/src/components/Form.js
+++ react_todo_app/src/components/Form.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Form = () => {
+const Form = ({setInputText}) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value)
+  }
+  const submitTodoHandler = (e) => {
+    e.preventDefault()
+  }
   return (
     <div>
       <form>
-        <input type="text" className="todo-input" />
-        <button className="todo-button" type="submit">
+        <input onChange={inputTextHandler} type="text" className="todo-input" />
+        <button onClick={submitTodoHandler} className="todo-button" type="submit">
           <i className="fas fa-plus-square"></i>
         </button>
         <div className="select">

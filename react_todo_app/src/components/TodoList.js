836f3a85494ec2d331ad158b452b8039921cec05
Kenneth Palces <kennypal@kenneths-mbp.myfiosgateway.com>
2021-02-25 07:22:43 -0500

    delete button

--- react_todo_app/src/components/TodoList.js
+++ react_todo_app/src/components/TodoList.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import Todo from './Todo'
+import React from "react";
+import Todo from "./Todo";
 
-const TodoList = ({todos}) => {
+const TodoList = ({ todos, setTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {todos.map(todo => (
-          <Todo key={todo.id} text={todo.text}/>
+        {todos.map((todo) => (
+          <Todo
+            todo={todo}
+            todos={todos}
+            setTodos={setTodos}
+            key={todo.id}
+            text={todo.text}
+          />
         ))}
       </ul>
     </div>
